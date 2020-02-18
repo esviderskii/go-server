c274c841c05927e8151da1fb63250ac68a6820c9
esviderskii <esviderskii@quantenna.com>
2020-02-18 16:28:02 +0300

    Final 0.0.1

--- main.go
+++ main.go
@@ -1,47 +1,58 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
+var port = flag.String("p", "8080", "port to serve on")
+var dir = flag.String("d", "/tmp", "the directory of static file to host")
+
+// UploadFile comment
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("File Upload Endpoint Hit")
+	fmt.Println("File Upload Endpoint Hit")
 
-    r.ParseMultipartForm(10 << 20)
-    file, handler, err := r.FormFile("myFile")
-    if err != nil {
-        fmt.Println("Error Retrieving the File")
-        fmt.Println(err)
-        return
-    }
-    defer file.Close()
-    fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-    fmt.Printf("File Size: %+v\n", handler.Size)
-    fmt.Printf("MIME Header: %+v\n", handler.Header)
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-    tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer tempFile.Close()
+	f, err := os.OpenFile(*dir+"/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 
-    fileBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        fmt.Println(err)
-    }
-    tempFile.Write(fileBytes)
-    fmt.Fprintf(w, "Successfully Uploaded File\n")
+	if err != nil {
+		fmt.Println("File not create")
+		fmt.Println(err)
+		return
+	}
+
+	// Copy the file to the destination path
+	io.Copy(f, file)
+
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
 
 func setupRoutes() {
-    http.HandleFunc("/upload", uploadFile)
-    http.ListenAndServe(":8080", nil)
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	http.HandleFunc("/upload", uploadFile)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
 func main() {
-    fmt.Println("Hello World")
-    setupRoutes()
+	flag.Parse()
+	log.Printf("Serving %s on HTTP port: %s\n", *dir, *port)
+	setupRoutes()
 }
-
