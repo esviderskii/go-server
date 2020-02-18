0b8780dae18dcde589893265ee882ca4c47af7f2
esviderskii <esviderskii@quantenna.com>
2020-02-18 10:03:01 +0300

    Edited main.go

--- main.go
+++ main.go
@@ -1,26 +1,46 @@
-/*
-Serve is a very simple static file server in go
-Usage:
-	-p="8100": port to serve on
-	-d=".":    the directory of static files to host
-Navigating to http://localhost:8100 will display the index.html or directory
-listing file.
-*/
 package main
 
 import (
-	"flag"
-	"log"
-	"net/http"
+    "fmt"
+    "io/ioutil"
+    "net/http"
 )
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("File Upload Endpoint Hit")
+
+    r.ParseMultipartForm(10 << 20)
+    file, handler, err := r.FormFile("myFile")
+    if err != nil {
+        fmt.Println("Error Retrieving the File")
+        fmt.Println(err)
+        return
+    }
+    defer file.Close()
+    fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+    fmt.Printf("File Size: %+v\n", handler.Size)
+    fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+    tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer tempFile.Close()
+
+    fileBytes, err := ioutil.ReadAll(file)
+    if err != nil {
+        fmt.Println(err)
+    }
+    tempFile.Write(fileBytes)
+    fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func setupRoutes() {
+    http.HandleFunc("/upload", uploadFile)
+    http.ListenAndServe(":8080", nil)
+}
+
 func main() {
-	port := flag.String("p", "8100", "port to serve on")
-	directory := flag.String("d", ".", "the directory of static file to host")
-	flag.Parse()
-
-	http.Handle("/", http.FileServer(http.Dir(*directory)))
-
-	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
+    fmt.Println("Hello World")
+    setupRoutes()
 }
